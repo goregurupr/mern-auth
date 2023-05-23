18ab91d7c9f2c60f4c4469f39a6d3acc81f19099
prasad <prasad@gamil.com>
2023-05-23 10:06:07 +0530

    23-5-23

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const User = require("../models/User")
+const bcrypt = require("bcrypt")
+
+exports.register = async (req, res) => {
+    try {
+        const { password } = req.body
+
+        const hashPass = await bcrypt.hash(password, 10)
+        const result = await User.create({
+            ...req.body,
+            password: hashPass
+        }) 
+        res.json({
+            message: "user register success",
+            result
+        })
+    } catch (error) {
+        res.json({ message: "something went wrong", error })
+    }
+}
+
+exports.fetchUsers = async (req, res) => {
+    try {
+        const result = await User.find()  // get
+        res.json({
+            message: "user fetch success",
+            result
+        })
+    } catch (error) {
+        res.json({ message: "something went wrong", error })
+    }
+}
